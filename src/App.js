1ae2ace017e4e43e0de664d30a1b234641a63455
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2021-03-23 18:18:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 // import { Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 import Navbar from './Navbar'
-import "./assets/vendor/aos/aos.css"
-import "./assets/vendor/bootstrap/css/bootstrap.min.css" 
-import "./assets/vendor/bootstrap-icons/bootstrap-icons.css" 
-import "./assets/vendor/boxicons/css/boxicons.min.css" 
-import "./assets/vendor/glightbox/css/glightbox.min.css" 
-import "./assets/vendor/swiper/swiper-bundle.min.css"
+
+import "./assets/bootstrap/css/bootstrap.min.css" 
+import "./assets/bootstrap-icons/bootstrap-icons.css" 
+import "./assets/boxicons/css/boxicons.min.css" 
+
+
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
