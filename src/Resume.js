34173c3867ed337ece815a2a4c5a89701588d2b7
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2021-05-21 22:06:12 +0530

    u4

--- src/Resume.js
+++ src/Resume.js
@@ -16,7 +16,7 @@ function Resume(){
           <div className="col-lg-6" data-aos="zoom-in-up" data-aos-delay="200">
 
              <div className='bbb'>
-                <a href="https://drive.google.com/file/d/17L2DprzoLBb7SFjrDu8FxzbRoGMGAooj/view?usp=sharing" className="btnn btn-2 btn-2h"> <FaFileDownload size='30'/> &nbsp;Download Resume</a>
+                <a href="https://drive.google.com/drive/folders/1UjnYTGz0BNKj08DPXdTCuM7hrPBgtBhH?usp=sharing" className="btnn btn-2 btn-2h"> <FaFileDownload size='30'/> &nbsp;Download Resume</a>
             </div>
 
           </div>
